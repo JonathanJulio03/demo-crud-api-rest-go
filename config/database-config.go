fd134606e9a896917e84f984063237aa0f67a2f4
Jonathan <jonjulio03@gmail.com>
2021-08-05 23:05:49 -0500

    apirestGo

---
+++ config/database-config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/JonathanJulio03/demo-crud-api-rest-go/entity"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func SetupDatabaseConnection() *gorm.DB {
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		panic("Carga de archivo fallida")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Conexión a base de datos fallida")
+	}
+
+	db.AutoMigrate(&entity.Book{}, &entity.User{})
+	return db
+}
+
+func CloseDatabaseConnection(db *gorm.DB) {
+	dbSQL, err := db.DB()
+	if err != nil {
+		panic("Cerrar base de datos fallida")
+	}
+	dbSQL.Close()
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/JonathanJulio03/demo-crud-api-rest-go
+
+go 1.16
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-gonic/gin v1.7.3
+	github.com/joho/godotenv v1.3.0
+	github.com/mashingan/smapping v0.1.11 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	gorm.io/driver/mysql v1.1.1
+	gorm.io/gorm v1.21.9
+)
